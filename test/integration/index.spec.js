88b2b8dc54a209186be74a0b9f0dff20784126a0
Evan Lloyd <eulloyd@gmail.com>
2014-05-24 16:08:12 +0900

    Move to unit tests

--- test/integration/index.spec.js
+++ test/integration/index.spec.js
@@ -1,11 +1,26 @@
 describe('waitstaffCalc', function () {
-	var ptor = protractor.getInstance();
 
 	describe("index", function () {
 		it("should display the correct title", function () {
 
-			ptor.get('/#');
-			expect(ptor.getTitle()).toBe('Waitstaff Calculator');
+			browser.get('/#');
+			expect(browser.getTitle()).toBe('Waitstaff Calculator');
 		});
+
+		it("should display customer charges when input is valid", function(){
+
+			browser.get('/#');
+			element(by.model('input.mealPrice')).sendKeys('100');
+			element(by.model('input.taxRate')).sendKeys('5');
+			element(by.model('input.tipPercentage')).sendKeys('15');
+			element(by.id('submit')).click().then(function() {
+				expect(element(by.model('output.subtotal')).getText()).toBe('105');
+				expect(element(by.model('output.tip')).getText()).toBe('15');
+				expect(element(by.model('output.total')).getText()).toBe('120');
+			});
+
+
+		});
+
 	});
 });
