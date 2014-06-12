3335a8daa5e2c456abe14eafaa6d01aef309ad55
Evan <eulloyd@gmail.com>
2014-06-12 05:13:09 +0000

    Refactor earnings Ctrl

--- js/app.js
+++ js/app.js
@@ -16,12 +16,17 @@ angular.module('waitstaffCalc', ['ngRoute'])
 })
 
 .controller('tipCtrl', function($scope, tipData){
+  $scope.output = {};
+  
   $scope.setData = function(){
-    $scope.output = {};
     $scope.output.subtotal = ($scope.input.mealPrice + ($scope.input.mealPrice * $scope.input.taxRate/100));
     console.log($scope.output.subtotal);
     $scope.output.tip = ($scope.input.tipPercentage/100) *($scope.input.mealPrice);
     $scope.output.total = $scope.output.tip + $scope.output.subtotal;
+    tipData.tip = $scope.output.tip;
+    tipData.count = ++tipData.count;
+    tipData.total = tipData.total + $scope.output.tip;
+    console.log(tipData);
   }
   
   $scope.clearInput = function(){
@@ -31,9 +36,23 @@ angular.module('waitstaffCalc', ['ngRoute'])
 })
 
 .controller('earningsCtrl', function($scope, tipData){
+  $scope.earnings = {};
+  $scope.earnings.tips = tipData.total;
+  $scope.earnings.meals = tipData.count;
+  $scope.earnings.average = $scope.earnings.tips / $scope.earnings.meals;
   
+  $scope.clearAll = function(){
+    tipData.tip = 0;
+    tipData.count = 0;
+    tipData.total = 0;
+    $scope.earnings = '';
+  };
 })
 
 .service('tipData', function(){
-	return {};
+	return {
+    tip: 0,
+    count: 0,
+    total: 0
+  };
 });
